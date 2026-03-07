fa699387dc34d6a69c7c9ec149c00c4de1f7b9b3
SamRiess <Samuel.Riess@outlook.de>
2026-03-07 20:38:39 +0100

    final analysis
    Finally the code is working. The whole simulation is working and i can use the p-values to write my report.

--- simulation/01_base.R
+++ simulation/01_base.R
@@ -222,12 +222,14 @@ ggplot(plot_data_IE, aes(x = INC, y = IE, color = LE, group = LE)) +
 #' 
 #' @return (IE) individual effort. Ranges from 0 to 1 (continuous)
 
-get_super_IE <- function(CT, S, LE) {
+get_super_IE <- function(CT, S, EMC) {
+  LE <- get_LE(EMC)
   CON <- get_CON(CT) 
   P <- get_P(CT, S)
   INC <- get_INC(CON)
   IE <- get_IE(INC, P, LE)
-  IE <- IE + rnorm(1, mean = 0, sd = 0.3)
+  
+  IE <- IE + rnorm(length(IE), mean = 0, sd = 0.3)
   IE[IE > 1] <- 1
   IE[IE < 0] <- 0
   return(IE)
@@ -285,8 +287,8 @@ ggplot(plot_data_IO, aes(x = MC, y = IO)) +
 #' 
 #' @return (IO) individual outcome. Ranges from 0 to inf (continuous)
 
-get_super_IO <- function(CT, S, MC, LE){
-  IO <- get_super_IE(CT, S, LE) * MC 
+get_super_IO <- function(CT, S, MC, EMC){
+  IO <- get_super_IE(CT, S, EMC) * MC 
   return (IO)
 }
 
@@ -324,7 +326,7 @@ df <- expand.grid(
   id = 1:n, 
   CT = c(0, 1, 5),
   S = c(2),
-  EMC = c(1, 2, 3) # HE, SLR, LE
+  EMC = c(1, 2, 3) # LE, SLR, HE
 )
 
 # determine the variables for a simulation of the exogenous variables
@@ -349,7 +351,7 @@ df <- merge(df, max_per_person, by = "id")
 df$IE <- get_super_IE(
   CT = df$CT,
   S = df$S, 
-  LE = df$LE
+  EMC = df$EMC
 )
 
 # compute the manifest outcome variable (IO)
@@ -357,7 +359,7 @@ df$IO <- get_super_IO(
   CT = df$CT,
   S = df$S,
   MC = df$maximum, 
-  LE = df$LE
+  EMC = df$EMC
 )
 
 # look at the simulated data 
@@ -365,28 +367,29 @@ df$IO <- get_super_IO(
 
 #compute the means of IO for each experimental group
 data_final <- df %>%
-  group_by(CT, LE) %>%
+  group_by(CT, EMC) %>%
   summarise(means = mean(IO), .groups = "drop")
 
 
-plot_final <- ggplot(data_final, aes(x = CT, y = means, color = factor(LE))) +
+plot_final <- ggplot(data_final, aes(x = CT, y = means, color = factor(EMC))) +
   geom_point(shape = 8) +
   labs(
     title = "Mean Individual Outcome (dyn/cm^2)", 
     subtitle = "Dependent on experimental group (S = 1)", 
     y = "Sound Pressure in dyn percm^2", 
     x = "conditions (Number of Cotargets)",
-    color = "LE"
+    color = "EMC"
   ) +
   scale_x_continuous(limits = c(0, 5), breaks = c(0, 1, 5)) +
   theme_bw() +
   geom_line()
 plot_final
 
-## Variance analysis for the replication of Latanße (1979)
+## Variance analysis for the replication of Latane (1979)
 # we have to set the LE condition to 0.7, cause Effort Matching wasn't integrated in this paper
-df_Latane <- subset(df, LE == 0.7)
+df_Latane <- subset(df, EMC == 2)
 
+#define the CT-factor
 df_Latane$CT_factor <- factor(df_Latane$CT,
                        levels = c(0,1,5),
                        labels = c("CT0","CT1","CT5"))
@@ -425,11 +428,35 @@ df_harkins$CT_factor <- factor(df_harkins$CT,
                                labels = c("Alone", "Pair"))
 
 #define the LE-factor
-df_harkins$LE_factor <- factor(df_harkins$LE,
-                               levels = c(0.5, 0.7, 0.9),
+df_harkins$EMC_factor <- factor(df_harkins$EMC,
+                               levels = c(1, 2, 3),
                                labels = c("Low", "SLR", "High"))
 
-fit_harkins <- aov(IO ~ CT_factor * LE_factor, data = df_harkins)
+fit_harkins <- aov(IO ~ CT_factor * EMC_factor, data = df_harkins)
 summary(fit_harkins)
+
+#testing each pair between 0 and 1 Cotarget for each EMC condition
+
+#preparation: create a combined group for contrasts
+df_harkins$group <- interaction(df_harkins$CT_factor, df_harkins$EMC_factor)
+
+#new model, based on the new groups
+fit_harkins_uni <- aov(IO ~ group, data = df_harkins)
+
+#define a hypothesis for each pair
+hyp_low    <- "Alone.Low - Pair.Low = 0"
+hyp_noinfo <- "Alone.SLR - Pair.SLR = 0"
+hyp_high   <- "Alone.High - Pair.High = 0"
+
+hyps_harkins <- c(hyp_low, hyp_noinfo, hyp_high)
+
+kontraste_harkins <- mcp(group = hyps_harkins)
+fit_final <- glht(fit_harkins_uni, linfct = kontraste_harkins)
+
+#show results
+summary(fit_final, test = univariate())
+confint(fit_final)
+
+
 
 
